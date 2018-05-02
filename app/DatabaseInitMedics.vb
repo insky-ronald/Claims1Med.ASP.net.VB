0c825366bbf6b6bdfd8156a906d1f006269f254b
john-zapanta <35802297+john-zapanta@users.noreply.github.com>
2018-05-02 20:44:09 +0800

    remove duplicate entry in DatabaseInitMedics.vb

--- app/DatabaseInitMedics.vb
+++ app/DatabaseInitMedics.vb
@@ -306,18 +306,18 @@ With DBConnection.NewCommand("AddCurrencies", "AddCurrencies", CommandType.Store
 	.AddParameter("action_msg", SqlDbType.varchar, ParameterDirection.InputOutput, 200, "")
 End with 
 
-With DBConnection.NewCommand("GetAddressTypes", "GetAddressTypes", CommandType.StoredProcedure)
-    .AddParameter("code", SqlDbType.VarChar, ParameterDirection.Input, 3, "")
-    .AddParameter("filter", SqlDbType.varchar, ParameterDirection.Input, 100, "")
-	.AddParameter("action", SqlDbType.int, ParameterDirection.Input, 0, 0)
-    .AddParameter("page", SqlDbType.int, ParameterDirection.Input, 0, 1)
-    .AddParameter("pagesize", SqlDbType.int, ParameterDirection.Input, 0, 0)
-    .AddParameter("row_count", SqlDbType.int, ParameterDirection.InputOutput, 0, 0)
-    .AddParameter("page_count", SqlDbType.int, ParameterDirection.InputOutput, 0, 0)
-    .AddParameter("sort", SqlDbType.varchar, ParameterDirection.Input, 200, "code")
-    .AddParameter("order", SqlDbType.varchar, ParameterDirection.Input, 10, "asc")
-    .AddParameter("visit_id", SqlDbType.bigint, ParameterDirection.Input, 0, 0)
-End With
+' With DBConnection.NewCommand("GetAddressTypes", "GetAddressTypes", CommandType.StoredProcedure)
+    ' .AddParameter("code", SqlDbType.VarChar, ParameterDirection.Input, 3, "")
+    ' .AddParameter("filter", SqlDbType.varchar, ParameterDirection.Input, 100, "")
+	' .AddParameter("action", SqlDbType.int, ParameterDirection.Input, 0, 0)
+    ' .AddParameter("page", SqlDbType.int, ParameterDirection.Input, 0, 1)
+    ' .AddParameter("pagesize", SqlDbType.int, ParameterDirection.Input, 0, 0)
+    ' .AddParameter("row_count", SqlDbType.int, ParameterDirection.InputOutput, 0, 0)
+    ' .AddParameter("page_count", SqlDbType.int, ParameterDirection.InputOutput, 0, 0)
+    ' .AddParameter("sort", SqlDbType.varchar, ParameterDirection.Input, 200, "code")
+    ' .AddParameter("order", SqlDbType.varchar, ParameterDirection.Input, 10, "asc")
+    ' .AddParameter("visit_id", SqlDbType.bigint, ParameterDirection.Input, 0, 0)
+' End With
 
 ' With DBConnection.NewCommand("AddAddressTypes", "AddAddressTypes", CommandType.StoredProcedure)
 	' .AddParameter("code", SqlDbType.VarChar, ParameterDirection.InputOutput, 3, "")
